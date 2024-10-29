14ce45b16915b51fe761241c7919eb7a27980fe9
Ayushi Singh <ayushisingh952891@gmail.com>
2024-10-29 21:53:12 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,25 +5,25 @@ import Navigation from "@/components/navigation";
 import { Earth } from "@/components/models/Earth";
 import Astronaut1 from "@/components/models/Astronaut1";
 
-
-
-
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image priority sizes="100vw" src={bg} alt="background-image" fill className="w-full h-full object-cover object-center opacity-25" />
-    <div className="w-full h-screen">
-    {/* navigation and 3d model */}
-    <Navigation/>
-    <RenderModel>
-   
-    
-    <Earth/>
-    <Astronaut1/>
-    </RenderModel>
-    </div>
-
+      <Image
+        priority
+        sizes="100vw"
+        src={bg}
+        alt="background-image"
+        fill
+        className="w-full h-full object-cover object-center opacity-25"
+      />
+      <div className="w-full h-screen">
+        {/* navigation and 3d model */}
+        <Navigation />
+        <RenderModel>
+          <Earth />
+          <Astronaut1 />
+        </RenderModel>
+      </div>
     </main>
   );
 }
